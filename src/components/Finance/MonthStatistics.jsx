9af63013e0954f6b78559ae0448d28a3741e1e7b
altynbeq <starknbek@gmail.com>
2024-07-16 02:19:19 +0500

    new patch 2

--- src/components/Finance/MonthStatistics.jsx
+++ src/components/Finance/MonthStatistics.jsx
@@ -74,7 +74,7 @@ const MonthStatistics = (monthFinanceData) => {
     ];
 
     return (
-        <div className="bg-white dark:text-gray-200 justify-center align-center text-center dark:bg-secondary-dark-bg p-1 ml-1 w-[90%] md:w-[25%] rounded-2xl">
+        <div className="bg-white dark:text-gray-200 justify-center align-center text-center dark:bg-secondary-dark-bg p-1 ml-1 w-[90%] md:w-[30%] rounded-2xl">
             <div className="flex flex-wrap justify-center">
                 <div className="md:w-400 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6 m-3">
                 <div className="flex justify-between">
